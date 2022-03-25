bf59dad496a25a2c75e2ba6e84bce7d877208c6f
Zeroge <fernandezdiego3758@gmail.com>
2022-03-25 12:38:01 +0100

    Ejercicio 2
    Co-authored-by: Ineesdv <ineesdv@gmail.com>
    Co-authored-by: allbaa07 <alba100701@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # This is a sample Python script.
+import numpy as np
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 import json
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -16,13 +18,17 @@ def hello():
     con.execute("PRAGMA foreign_keys = 1")
     cur = con.cursor()
     cur.execute(
-        'CREATE TABLE IF NOT EXISTS legal(url text primary key, cookies boolean, aviso boolean, proteccion_de_datos boolean, creacion number)')
+        'CREATE TABLE IF NOT EXISTS legal(url text primary key, cookies boolean, aviso boolean, proteccion_de_datos '
+        'boolean, creacion number)')
     cur.execute(
-        'CREATE TABLE IF NOT EXISTS users(name text primary key, telefono text, contrasena text, provincia text, permisos boolean)')
+        'CREATE TABLE IF NOT EXISTS users(name text primary key, telefono text, contrasena text, provincia text, '
+        'permisos boolean)')
     cur.execute(
-        'CREATE TABLE IF NOT EXISTS emails(name text, total number, phising number, clicados number, foreign key (name) references users (name))')
-    cur.execute('CREATE TABLE IF NOT EXISTS ips(name text, ip text, foreign key (name) references users (name))')
-    cur.execute('CREATE TABLE IF NOT EXISTS fechas(name text , fecha text, foreign key (name) references users (name))')
+        'CREATE TABLE IF NOT EXISTS emails(name text, total number, phising number, clicados number, foreign key ('
+        'name) references users (name))')
+    cur.execute('CREATE TABLE IF NOT EXISTS ips(name text, ip text, fecha text , foreign key (name) references users ('
+                'name))')
+
     con.commit()
     cur.close()
     return "<p>Hello World</p>"
@@ -51,13 +57,13 @@ def func():
         cur.execute(
             'INSERT INTO legal VALUES("{}", "{}", "{}", "{}", "{}")'.format(url, cookies, aviso, proteccion_de_datos,
                                                                             creacion))
-        print("name: {}\nCookies: {}\nAviso: {}\nProteccion de Datos: {}\ncreacion: {}\n".format(url, cookies, aviso,
-                                                                                                 proteccion_de_datos,
-                                                                                                 creacion))
+        # print("name: {}\nCookies: {}\nAviso: {}\nProteccion de Datos: {}\ncreacion: {}\n".format(url, cookies, aviso,
+        #                                                                                          proteccion_de_datos,
+        #                                                                                          creacion))
     f.close()
     f = open("data/users.json")
     data = json.load(f)
-    print(data)
+    # print(data)
     for entry in data["usuarios"]:
         name = list(entry.keys())[0]
         telefono = entry[name]["telefono"]
@@ -71,16 +77,19 @@ def func():
                     .format(name, telefono, contrasena, provincia, permisos))
         cur.execute('INSERT INTO emails values("{}", "{}", "{}", "{}")'
                     .format(name, emails["total"], emails["phishing"], emails["clicados"]))
-        for ip in ips:
-            cur.execute('INSERT INTO ips values("{}", "{}")'
-                        .format(name, ip))
-            pass
-        for fecha in fechas:
-            cur.execute('INSERT INTO fechas values("{}", "{}")'
-                        .format(name, fecha))
-        print(
-            "Name: {}\nTelefono: {}\nContrasena: {}\nProvincia: {}\nPermisos: {}\nEmails: {}\nFechas: {}\nIps: {}\n".format(
-                name, telefono, contrasena, provincia, permisos, emails, fechas, ips))
+        if len(ips) == len(fechas) and len(fechas) > 0 and ips is not None:
+            for i in range(len(fechas)):
+                cur.execute('INSERT INTO ips values("{}", "{}", "{}")'
+                            .format(name, ips[i], fechas[i]))
+                pass
+        else:
+            for i in range(len(fechas)):
+                cur.execute('INSERT INTO ips values("{}", "{}", "{}")'
+                            .format(name, None, fechas[i]))
+                pass
+        # print(
+        #     "Name: {}\nTelefono: {}\nContrasena: {}\nProvincia: {}\nPermisos: {}\nEmails: {}\nFechas: {}\nIps: {}\n".format(
+        #         name, telefono, contrasena, provincia, permisos, emails, fechas, ips))
         pass
     con.commit()
     cur.close()
@@ -92,13 +101,51 @@ def func():
 def dataframe():
     con = sqlite3.connect("example2.db", timeout=10)
     # con.execute("PRAGMA foreign_keys = 1")
-    cur = con.cursor()
-    print(cur.execute('SELECT COUNT(*) FROM users'))
-    print(cur.execute('SELECT COUNT(*) FROM users'))
-    print(cur.fetchmany(1))
-    usuarios = cur.fetchall()[0][0]
-    cur.close()
-    return "numero de usuarios = " + str(usuarios)
+    # cur = con.cursor()
+    query = pd.read_sql_query(
+        'SELECT u.*, i.ip, i.fecha, e.total, e.phising, e.clicados FROM users u JOIN ips i ON u.name=i.name '
+        'JOIN emails e ON u.name=e.name WHERE u.provincia != "None" AND '
+        'u.telefono != "None" AND u.name LIKE "%.%" AND i.ip LIKE "%.%.%.%"', con, "name")
+    datafreim = pd.DataFrame(query,
+                             columns=["name", "telefono", "contrasena", "provincia", "permisos", "ip", "fecha", "total",
+                                      "phising", "clicados"])
+    print(datafreim)
+    datafreim = datafreim.drop("name", axis=1)
+    print(datafreim)
+    original = datafreim.groupby(["name"], dropna=True).agg(
+        {"fecha": np.array, "ip": np.array, "telefono": "first", "contrasena": "first", "provincia": "first",
+         "permisos": "first", "total": "first", "phising": "first", "clicados": "first"})
+
+    fecha = datafreim.groupby(["name"], dropna=True).agg(
+        {"fecha": "count"})
+    ips = datafreim.groupby(["name"], dropna=True).agg(
+        {"ip": "count"})
+    emails = datafreim.groupby(["name"], dropna=True).agg(
+        {"total": "first"})
+    mediafecha = fecha.mean()["fecha"]
+    desviacionfecha = fecha.std()["fecha"]
+    mediaips = ips.mean()["ip"]
+    desviacionips = ips.std()["ip"]
+    mediaemails = emails.mean()["total"]
+    desviacionemails = emails.std()["total"]
+    usuarios = original.shape[0]
+    maxfecha = fecha.max()["fecha"]
+    minfecha = fecha.min()["fecha"]
+    maxemails = emails.max()["total"]
+    minemails = emails.min()["total"]
+
+    # print(datafreim)
+    # cur.execute('SELECT COUNT(*) FROM users u JOIN ips i ON u.name=i.name WHERE u.provincia != "None" AND '
+    #             'u.telefono != "None" AND u.name LIKE "%.%" AND i.ip LIKE "%.%.%.%"')
+    # usuarios = cur.fetchall()[0][0]
+    # print(usuarios)
+    # cur.close()
+    con.close()
+    return "<p>Numero de usuarios = " + str(usuarios)+\
+           "<br>Fecha:<ul><li>Media = "+str(mediafecha)+"</li><li>Desviacion = "\
+           +str(desviacionfecha)+"</li><li>Maximo = "+str(maxfecha)+"</li><li>Minimo = "+str(minfecha)+"</li></ul>IPs:<ul><li>Media = "+str(mediaips)+"</li><li>Desviacion = "\
+           +str(desviacionips)+"</li></ul>Emails:<ul><li>Media = "+str(mediaemails)+"</li><li>Desviacion = "+str(desviacionemails)+"</li><li>Maximo = "\
+           +str(maxemails)+"</li><li>Minimo = "+str(minemails)+"</li></ul></p>"
 
 
 # Press the green button in the gutter to run the script.
